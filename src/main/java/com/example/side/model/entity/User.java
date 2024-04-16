4b331093ef3e7ed0d28c5f2475115b89daa9b1de
minseid <mintang0117@gmail.com>
2024-04-16 19:09:20 +0900

    setting

--- src/main/java/com/example/side/model/entity/User.java
+++ src/main/java/com/example/side/model/entity/User.java
@@ -1,4 +1,32 @@
 package com.example.side.model.entity;
 
+import jakarta.persistence.*;
+import lombok.*;
+import lombok.experimental.Accessors;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Data // Lombok 어노테이션: getter, setter, toString, equals, hashCode 메서드를 자동 생성합니다.
+@Entity // 데이터베이스 테이블과 매핑되는 엔티티 클래스임을 나타냅니다.
+@AllArgsConstructor // 모든 필드를 인자로 받는 생성자를 자동 생성합니다.
+@NoArgsConstructor // 파라미터가 없는 생성자를 자동 생성합니다.
+@Builder // 빌더 패턴을 구현합니다.
+@Accessors(chain = true) // setter 메서드를 체인 형태로 사용할 수 있도록 합니다.
+@ToString(exclude = {}) // 해당 필드를 toString 메서드에서 제외합니다.
+@EntityListeners(AuditingEntityListener.class) // 엔티티의 변경 사항을 자동으로 감지합니다.
+
 public class User {
+
+    @Id // 엔티티의 주키(primary key)를 나타냅니다.
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 자동 생성되는 값의 전략을 지정합니다. (예: auto_increment)
+    private int id;
+    private String name;
+    private String profile;
+    private String Nickname;
+    private String description;
+
+    private String Kakao;
+    private String Google;
+    private String Naver;
+
+
 }
--- src/main/java/com/example/side/service/UserService.java
+++ src/main/java/com/example/side/service/UserService.java
@@ -1,4 +1,13 @@
 package com.example.side.service;
 
-public class UserService {
+import com.example.side.model.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserService extends JpaRepository<User, Long> {
+    Page<User> findall(Pageable pageable);
 }
